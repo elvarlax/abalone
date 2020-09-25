3c5876dd6bb4956df7f06d6cfd6bcf03f5639985
Elvar Laxdal <elvarlax@gmail.com>
2020-09-25 17:46:14 +0200

    Auto stash before merge of "master" and "origin/master"

--- abalone.py
+++ abalone.py
@@ -4,23 +4,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.linalg as linalg
 import categoric2numeric as c2n
+from IPython import get_ipython
 
-get_ipython().run_line_magic('matplotlib', 'qt')
+# get_ipython().run_line_magic('matplotlib', 'qt')
 
 # Importing the dataset
 dataset = pd.read_csv('abalone.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Basic summary statistics
-# print(dataset)
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Printing X_test
-print(X_test)
 
 # Printing X_train
 print(X_train)
@@ -43,44 +40,43 @@ X = np.hstack((MFI,Y))
 def PCAAnalysis(Y,y,MFI):
     L = len(Y[0])
     Y.dtype = np.float
-    Y = Y - np.ones((len(Y),1))*Y.mean(0)
-    U,S,V = linalg.svd(Y,full_matrices=False)
-    
-    rho = S/sum(S)
-    rhoa = np.zeros((len(S),),float)
+    Y = Y - np.ones((len(Y), 1)) * Y.mean(0)
+    U, S, V = linalg.svd(Y, full_matrices=False)
+
+    rho = S / sum(S)
+    rhoa = np.zeros((len(S),), float)
     for i in range(len(rho)):
         rhoa[i] = sum(rho[:i])
-    
+
     K = 7;
     plt.figure()
-    plt.plot(rhoa,'o-')
-    plt.plot(rho,'o-')
-    #plt.legend(["","Variation explained by the given principle component"])
+    plt.plot(rhoa, 'o-')
+    plt.plot(rho, 'o-')
+    # plt.legend(["","Variation explained by the given principle component"])
     plt.show()
-    
-    Shat = np.vstack((np.diag(S[0:K]),np.zeros((L-K,K),float)))
-    Vhat = V[0:K,:]
-    #PC = np.dot(D,V[:,0:2])
-    Xhat = np.dot(U,Shat)
+
+    Shat = np.vstack((np.diag(S[0:K]), np.zeros((L - K, K), float)))
+    Vhat = V[0:K, :]
+    # PC = np.dot(D,V[:,0:2])
+    Xhat = np.dot(U, Shat)
     plt.figure()
-    plt.plot(Xhat[:,0],Xhat[:,1],'o')
+    plt.plot(Xhat[:, 0], Xhat[:, 1], 'o')
     plt.xlabel("PCA #1")
-    plt.ylabel("PCA #2")       
+    plt.ylabel("PCA #2")
     plt.show()
-    
+
     plt.figure()
-    plt.plot(Xhat[:,2],Xhat[:,3],'o')
+    plt.plot(Xhat[:, 2], Xhat[:, 3], 'o')
     plt.xlabel("PCA #3")
-    plt.ylabel("PCA #4")       
+    plt.ylabel("PCA #4")
     plt.show()
     
     plt.figure()
     plt.plot(age,Xhat[:,0],'o')
     plt.xlabel("Rings")
-    plt.ylabel("PCA #1")       
+    plt.ylabel("PCA #1")
     plt.show()
-    
-    
+
     plt.figure()
     for i in range(len(age)):
         if MFI[i] == 'M':
@@ -91,10 +87,9 @@ def PCAAnalysis(Y,y,MFI):
             st = '+'
         plt.plot(Xhat[i,0],Xhat[i,1],st,color = (age[i]/33,0,1-age[i]/33))
     plt.xlabel("PCA #1")
-    plt.ylabel("PCA #2")       
+    plt.ylabel("PCA #2")
     plt.show()
-    
-    
+
     plt.figure()
     for i in range(len(age)):
         if MFI[i] == 'M':
@@ -105,21 +100,21 @@ def PCAAnalysis(Y,y,MFI):
             st = '+'
         plt.plot(Xhat[i,2],Xhat[i,3],st,color = (age[i]/33,0,1-age[i]/33))
     plt.xlabel("PCA #3")
-    plt.ylabel("PCA #4")       
+    plt.ylabel("PCA #4")
     plt.show()
     
     return V
 
-V = PCAAnalysis(X,age,MFIstr)
-
 def boxplot(dataset):
-    attributeNames = dataset.columns.tolist()
-    data = dataset.iloc[:, :-1]
+    attributeNames = dataset.columns
+    data = dataset.iloc[:, 1:-1]
     plt.boxplot(data)
-    xtricks(range(1.5).attributeNames)
-    ylabel('cm')
-    title('Boxplot')
-    show()
-    
-    
+    plt.xticks(range(1, 6), attributeNames)
+    plt.ylabel('cm')
+    plt.title('Boxplot')
+    plt.show()
+
+
+# PCAAnalysis(X)
+
 boxplot(dataset)
