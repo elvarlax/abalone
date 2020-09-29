961c77fc8ad68430e1cebbb96f02a766f5825a8d
Gísli Björn Helgason <s203357@student.dtu.dk>
2020-09-29 19:53:01 +0200

    Update abalone.py

--- abalone.py
+++ abalone.py
@@ -101,22 +101,105 @@ def matrix_plot(data):
     ax = sns.pairplot(data,hue='Sex', palette="Set1", markers = ["s","o","D"])
     plt.show()
 
-def similarity_analysis(X,Y, attribute_names):
-    method='cor'
-    similar_mat= np.zeros((len(X), len(X)), float)
-    for attribute in attribute_names:
-        for attribute in attribute_names:
-            sim(X, Y, method)
+def similarity_analysis(X,M,method='cor'):
+    sim_mat= np.zeros((M, M), float)
+    for j in range(M):
+        for i in range(M):
+            sim_mat[i,j]=sim.similarity(X.iloc[:,i],X.iloc[:,j],method)
+    #heatmap_plot(sim_mat)
+    return sim_mat
 
+def heatmap_plot(corr_data):
+    plt.figure()
+    #masking unique values
+    mask = np.zeros_like(corr_data)
+    mask[np.triu_indices_from(mask)] = True    
+    sns.heatmap(corr_data,mask=mask,xticklabels=attributeNames, yticklabels=attributeNames,square=True,
+                cbar_kws={'label': 'Correlation'})
+    plt.xlabel('Attribute name')
+    plt.ylabel('Attribute name')
+    plt.show()
+    
+
+def correlation_plot(d):
+  nrows,ncols=int(len(d)/2),int(len(d)/2)
+  fig, ax = plt.subplots(nrows, ncols,sharex=True,sharey=True)
+  k=0
+  for i in range(nrows):
+    for j in range(ncols):
+      key=list(d.keys())[k]
+      corr_data=d[key]
+      mask = np.zeros_like(corr_data)
+      mask[np.triu_indices_from(mask)] = True
+      if k==0 or k==2:
+          sns.heatmap(corr_data,mask=mask,xticklabels=attributeNames, yticklabels=attributeNames,square=True,ax=ax[i, j])
+      else:
+          sns.heatmap(corr_data,mask=mask,xticklabels=attributeNames, yticklabels=attributeNames,square=True,
+                cbar_kws={'label': 'Correlation'},ax=ax[i, j])
+      #heatmap_plot(d[key])
+      if k==0:
+        #ax[i,j].legend(loc='upper right')
+        ax[i,j].set_ylabel('Attribute name')
+      if i==1:
+        ax[i,j].set_xlabel('Attribute name')
+      if j==0:
+          ax[i,j].set_ylabel('Attribute name')
+      ax[i,j].set_title(key,size=12)
+      k+=1
+
+  #fig.tight_layout()
+  fig.show()
+    
+def data_analysis(dataset):
+    global attributeNames
+    attributeNames = np.asarray(dataset.columns[1:])
+    X = dataset.iloc[:, 1:]
+    classLabels = dataset.iloc[:,0]
+    #unique class labels 
+    classNames = np.unique(classLabels)
+    #class dictionary
+    classDict = dict(zip(classNames,range(len(classNames))))
+    #This is the class index vector y:
+    y = np.array([classDict[cl] for cl in classLabels])
+    #number of data objects and number of attributes 
+    N, M = X.shape
+    # The number of classes, C:
+    C = len(classNames)
+    
+    
+    corr_all=similarity_analysis(X,M)
+    d = {'All':corr_all}
+    for c in range(C):
+    # select indices belonging to class c:
+        class_mask = y==c
+        d[classNames[c]]=similarity_analysis(X.iloc[class_mask],M)
+        
+    
+    correlation_plot(d)
+        
+    #return X, C,y
+    
+    
+    
+def visual_data(dataset):
+    
+    df = dataset[dataset.columns[1:-1]]
+    box_plot(dataset['Sex'], dataset['Age'], df)
+    matrix_plot(df)
+    
+    
 if __name__ == "__main__":
     # Import dataset
     dataset = pd.read_csv('abalone.csv') 
-    X = dataset.iloc[:, :-1].values
-    y = dataset.iloc[:, -1].values
-    attributeNames = np.asarray(dataset.columns[:-1])
+    
     # Create a age column from the Rings column + 1.5
     dataset['Age'] = dataset['Rings'] + 1.5
     dataset.drop('Rings', axis=1, inplace=True)
+    
+############# put this in a pca function that manages the pca analysis??????????????????  
+    X = dataset.iloc[:, :-1].values
+
+    y = dataset.iloc[:, -1].values
 
     # Move this to a function
     Y = np.zeros((len(X), len(X[1]) - 1), float)
@@ -131,14 +214,17 @@ if __name__ == "__main__":
     MFIstr = X[:, 0]
     MFI, b = c2n.categoric2numeric(X[:, 0])
     X = np.hstack((MFI, Y))
+    
+    ###############################function calling
 
     # pca(Y, y, MFI)
-
-    #df = dataset[dataset.columns[1:-1]]
-    box_plot(dataset['Sex'], dataset['Age'], df)
-    matrix_plot(dataset)
         
-    pca(X,age, MFIstr)
+    #pca(X,age, MFIstr)
+    
+    #visual_data(dataset)
+
+    #data_analysis(dataset)
+
 
 
     
