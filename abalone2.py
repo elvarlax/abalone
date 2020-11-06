e64385a284e455f65a6499d4b0702d9c8c2ab720
Gísli Björn Helgason <s203357@student.dtu.dk>
2020-11-06 17:02:12 +0100

    Update abalone2.py

--- abalone2.py
+++ abalone2.py
@@ -11,6 +11,90 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 from toolbox_02450 import train_neural_net
 
+def lin_reg(x_train,y_train,x_test,y_test):
+    N, M = x_train.shape
+    x_train = np.concatenate((np.ones((x_train.shape[0],1)),x_train),1)
+    M = M+1
+    param = np.power(10.,range(-10,9)) 
+    opt_val_err, p, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(x_train, y_train, param, 10)
+    # Estimate weights for the optimal value of lambda, on entire training set
+    lambdaI = p* np.eye(M)
+    lambdaI[0,0] = 0 # Do no regularize the bias term
+    Xty = x_train.T @ y_train
+    XtX = x_train.T @ x_train
+    w_rlr = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+    # Compute mean squared error with regularization with optimal lambda
+    Error_train_rlr = np.square(y_train-x_train @ w_rlr).sum(axis=0)/y_train.shape[0]
+    err = np.square(y_test-x_test @ w_rlr).sum(axis=0)/y_test.shape[0]
+    return p,err
+
+def rlr_validate(X,y,lambdas,cvf=10):
+    ''' Validate regularized linear regression model using 'cvf'-fold cross validation.
+        Find the optimal lambda (minimizing validation error) from 'lambdas' list.
+        The loss function computed as mean squared error on validation set (MSE).
+        Function returns: MSE averaged over 'cvf' folds, optimal value of lambda,
+        average weight values for all lambdas, MSE train&validation errors for all lambdas.
+        The cross validation splits are standardized based on the mean and standard
+        deviation of the training set when estimating the regularization strength.
+        
+        Parameters:
+        X       training data set
+        y       vector of values
+        lambdas vector of lambda values to be validated
+        cvf     number of crossvalidation folds     
+        
+        Returns:
+        opt_val_err         validation error for optimum lambda
+        opt_lambda          value of optimal lambda
+        mean_w_vs_lambda    weights as function of lambda (matrix)
+        train_err_vs_lambda train error as function of lambda (vector)
+        test_err_vs_lambda  test error as function of lambda (vector)
+    '''
+    CV = model_selection.KFold(cvf, shuffle=True)
+    M = X.shape[1]
+    w = np.empty((M,cvf,len(lambdas)))
+    train_error = np.empty((cvf,len(lambdas)))
+    test_error = np.empty((cvf,len(lambdas)))
+    f = 0
+    y = y.squeeze()
+    for train_index, test_index in CV.split(X,y):
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        
+        # Standardize the training and set set based on training set moments
+        #mu = np.mean(X_train[:, 1:], 0)
+        #sigma = np.std(X_train[:, 1:], 0)
+        
+        #X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+        #X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
+        
+        # precompute terms
+        Xty = X_train.T @ y_train
+        XtX = X_train.T @ X_train
+        for l in range(0,len(lambdas)):
+            # Compute parameters for current value of lambda and current CV fold
+            # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
+            lambdaI = lambdas[l] * np.eye(M)
+            lambdaI[0,0] = 0 # remove bias regularization
+            w[:,f,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+            # Evaluate training and test performance
+            train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).mean(axis=0)
+            test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).mean(axis=0)
+    
+        f=f+1
+
+    opt_val_err = np.min(np.mean(test_error,axis=0))
+    opt_lambda = lambdas[np.argmin(np.mean(test_error,axis=0))]
+    train_err_vs_lambda = np.mean(train_error,axis=0)
+    test_err_vs_lambda = np.mean(test_error,axis=0)
+    mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
+    
+    return opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
+    
+        
+
 
 def knn(x_train, y_train, x_test, y_test, param):
     # Training the K-NN model on the Training set
@@ -102,7 +186,8 @@ def models(x_train, y_train, x_test, y_test, model_indices):
         model = knn
         param = (1, 5, 10)
     elif model_indices == "lin":
-        pass
+        p,err=lin_reg(x_train,y_train,x_test,y_test)
+        return p,err
     elif model_indices == "log":
         pass
     else:
